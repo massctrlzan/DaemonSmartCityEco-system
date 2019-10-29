995445ce1e0d373e3d681e16ae589fce29ea84f9
massctrlzan <46210266+massctrlzan@users.noreply.github.com>
2019-10-29 11:01:25 -0400

    Update Questing_Macro_Service.py
    added methods

--- Questing_Macro_Service.py
+++ Questing_Macro_Service.py
@@ -3,6 +3,19 @@ class QUest: #or appropriation activity
     request = self.request
     obtainable_boolean = self.obtainable_boolean
     serviceable_boolean = self.servicable_boolean
-    particulars = self.particulars # [reward//, obtainable, servicable, ]
+    
+    particulars = self.particulars # [reward//, valuation, obtainable, servicable,  transferable_bool, responce_limit,  ]
     
     self.responce = None
+
+    
+    def close_request():
+      pass
+    
+    def open_request():
+      pass
+    
+    def value_ping_responce_ident():
+      pass
+    
+    def 
