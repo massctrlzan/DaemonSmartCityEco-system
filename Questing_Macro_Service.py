904e076c2be12c5368ab789c04a6c50385844460
massctrlzan <46210266+massctrlzan@users.noreply.github.com>
2019-10-29 11:09:26 -0400

    Update Questing_Macro_Service.py

--- Questing_Macro_Service.py
+++ Questing_Macro_Service.py
@@ -3,6 +3,7 @@ class QUest: #or appropriation activity
     request = self.request
     obtainable_boolean = self.obtainable_boolean
     serviceable_boolean = self.servicable_boolean
+    elevation_level = self.elevation_level
     
     particulars = self.particulars # [reward//, valuation, obtainable, servicable,  transferable_bool, responce_limit,  ]
     
@@ -18,4 +19,15 @@ class QUest: #or appropriation activity
     def value_ping_responce_ident():
       pass
     
-    def 
+    def change_reward():  #potentially replaced with upreward_ down_reward_
+      pass
+    
+    def obtainable_redistribution(): #for when some items are found in minimality and others are found at a later date or 
+                                     #potentially by other responce associates
+      pass
+    
+    def serviceable_redistribution():
+      pass
+    
+    def additional_information_request_from_responce():
+      pass
