c15ab27cca7bc5ca3350198a43ab25d76a2bc131
massctrlzan <46210266+massctrlzan@users.noreply.github.com>
2019-10-29 10:56:54 -0400

    Update Questing_Macro_Service.py
    updated Quest/Request :: class with __init__() functionality

--- Questing_Macro_Service.py
+++ Questing_Macro_Service.py
@@ -1,6 +1,8 @@
 class QUest: #or appropriation activity
   __init__():
     request = self.request
-    particulars = self.particulars # []
+    obtainable_boolean = self.obtainable_boolean
+    serviceable_boolean = self.servicable_boolean
+    particulars = self.particulars # [reward//, obtainable, servicable, ]
     
     self.responce = None
